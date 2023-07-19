8ac1e8ba3bf3bedcfedc5872b0b0510ec8c9a0f2
mehrshad <95561979+khakifirooz@users.noreply.github.com>
2023-07-19 16:59:52 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xe82efc3b460c8e1bbb58825d1ec7ce917eacddae";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
